9859bcf4ec846188ac5e2328d1c7ba299dc30fce
Chris Donaldson <chris.donaldson@shopify.com>
2015-03-11 14:43:26 +0000

    reformat the regex to multiple lines to make it easier to follow and subsequently correct

--- lib/asset_cloud/base.rb
+++ lib/asset_cloud/base.rb
@@ -8,7 +8,25 @@ module AssetCloud
 
   class Base
     cattr_accessor :logger
-    VALID_PATHS = /\A(([a-z0-9])|((\.?[\w\-\@])([\w\-\@]|[\ ][\w\-\@]|[\/][\w\-\@]|[\/][\.][\w\-\@]|[\.][\w\-\@]+)*))\z/i
+    VALID_PATHS = /\A
+      (
+        ([a-z0-9])            #Filename can be a single letter
+        |                     #OR it is many and follows the below rules
+        (
+          (\.?[\w\-\@])       #It can start with a dot but it must have a following character
+          (
+            [\w\-\@]          #You can have a letter without any following conditions
+            |
+            [\ ][\w\-\@]      #If there is a space you need to have a normal letter afterward
+            |
+            [\/][\w\-\@]      #If there is a slash you need to have a normal letter afterward
+            |
+            [\/][\.][\w\-\@]  #Though a slash could be followed by a dot so long as there is a normal letter afterward
+            |
+            [\.][\w\-\@]+     #A dot must be followed by one (or more) normal letters
+          )*                  #Zero to many of these combinations.
+        )
+      )\z/ix                   #Case insensitive
     MATCH_BUCKET = /^(\w+)(\/|$)/
 
     attr_accessor :url, :root
