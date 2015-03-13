3b5db2254f3392009b77586b41a2db300feac3c4
Chris Donaldson <chris.donaldson@shopify.com>
2015-03-13 13:52:09 -0400

    Revert "reduce the restrictiveness of the filename filter"

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -42,39 +42,8 @@ describe BasicCloud do
     lambda { @fs['./test']   }.should raise_error(AssetCloud::IllegalPath)
   end
 
-  it "should raise error when filename has trailing period" do
-    lambda { @fs['test.']           }.should raise_error(AssetCloud::IllegalPath)
-    lambda { @fs['/test/testfile.'] }.should raise_error(AssetCloud::IllegalPath)
-    lambda { @fs['test/directory/.']}.should raise_error(AssetCloud::IllegalPath)
-  end
-
-  it "should raise error when using with minus relative even after another directory" do
-    lambda { @fs['test/../test']      }.should raise_error(AssetCloud::IllegalPath)
-    lambda { @fs['test/../../test']   }.should raise_error(AssetCloud::IllegalPath)
-    lambda { @fs['test/../../../test']}.should raise_error(AssetCloud::IllegalPath)
-  end
-
-  it "should raise an error when using names with bizarre combinations of '.' and ' '" do
-    lambda { @fs['test. . . .. ... .. . ']   }.should raise_error(AssetCloud::IllegalPath)
-  end
-
-  it "should not raise an error when using directory names with spaces" do
-    @fs['files/ass ets/.DS_Store']
-  end
-
-  it "should not raise_error when using unusual but valid filenames" do
-    @fs['.DS_Store']
-    @fs['photograph.g']
-    @fs['_testfilename']
-    @fs['assets/.DS_Store']
-    @fs['assets/photograph.g']
-    @fs['a/_testfilename']
-    @fs['a']
-  end
-
   it "should allow sensible relative filenames" do
     @fs['assets/rails_logo.gif']
-    @fs['assets/rails_logo']
     @fs['assets/rails-2.gif']
     @fs['assets/223434.gif']
     @fs['files/1.JPG']
