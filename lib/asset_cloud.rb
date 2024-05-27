78798cdc69b1ea8aadada5c157ab865d8472d16f
George Ma <george.ma@shopify.com>
2024-05-27 14:40:52 -0400

    WIP: Autoload AssetCloud::S3Bucket

--- lib/asset_cloud.rb
+++ lib/asset_cloud.rb
@@ -16,9 +16,6 @@ require "asset_cloud/buckets/memory_bucket"
 require "asset_cloud/buckets/versioned_memory_bucket"
 require "asset_cloud/base"
 
-# S3
-require "asset_cloud/buckets/s3_bucket"
-
 # GCS
 require "asset_cloud/buckets/gcs_bucket"
 
@@ -29,38 +26,48 @@ require "asset_cloud/validations"
 
 require "asset_cloud/asset_extension"
 
-AssetCloud::Base.class_eval do
-  include AssetCloud::FreeKeyLocator
-  include AssetCloud::Callbacks
-  callback_methods :write, :delete
-end
+module AssetCloud
+  extend ActiveSupport::Autoload
 
-AssetCloud::Asset.class_eval do
-  include AssetCloud::Callbacks
-  callback_methods :store, :delete
-
-  include AssetCloud::Validations
-  callback_methods :validate
-  validate :valid_key
-
-  def execute_callbacks(symbol, args)
-    result = super
-    result && @extensions.all? { |ext| ext.execute_callbacks(symbol, args) }
+  eager_autoload do
+    autoload_under "buckets" do
+      autoload :S3Bucket
+    end
   end
 
-  protected
-
-  def valid_key_path?(key)
-    key =~ AssetCloud::Base::VALID_PATHS
+  Base.class_eval do
+    include FreeKeyLocator
+    include Callbacks
+    callback_methods :write, :delete
   end
 
-  private
+  Asset.class_eval do
+    include Callbacks
+    callback_methods :store, :delete
 
-  def valid_key
-    if key.blank?
-      add_error("key cannot be empty")
-    elsif !valid_key_path?(key)
-      add_error("#{key.inspect} contains illegal characters")
+    include Validations
+    callback_methods :validate
+    validate :valid_key
+
+    def execute_callbacks(symbol, args)
+      result = super
+      result && @extensions.all? { |ext| ext.execute_callbacks(symbol, args) }
+    end
+
+    protected
+
+    def valid_key_path?(key)
+      key =~ Base::VALID_PATHS
+    end
+
+    private
+
+    def valid_key
+      if key.blank?
+        add_error("key cannot be empty")
+      elsif !valid_key_path?(key)
+        add_error("#{key.inspect} contains illegal characters")
+      end
     end
   end
 end
