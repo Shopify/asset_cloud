d7b291783a0421b6e378397423b7bdfe272389af
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2026-07-23 13:56:47 -0400

    Autoload buckets and base
    Buckets should be autoloaded because they require asset libraries that
    may not be used by the host application. As asset cloud grows
    in size, it is also a good approach to adopt as to not require too many
    files at boot time when, in many cases, asset cloud will not be used.
    
    Base is large enough that it should be autoloaded as well.

--- lib/asset_cloud.rb
+++ lib/asset_cloud.rb
@@ -14,26 +14,29 @@ require "asset_cloud/buckets/file_system_bucket"
 require "asset_cloud/buckets/invalid_bucket"
 require "asset_cloud/buckets/memory_bucket"
 require "asset_cloud/buckets/versioned_memory_bucket"
-require "asset_cloud/base"
 
-# S3
-require "asset_cloud/buckets/s3_bucket"
+# Core entry-point class and external-SDK backends are autoloaded so that
+# requiring asset_cloud does not load asset_cloud/base or pull in aws-sdk-s3
+# (or the GCS SDK) at boot. They are eager-loaded in production by the Railtie.
+module AssetCloud
+  autoload :Base, "asset_cloud/base"
+  autoload :S3Bucket, "asset_cloud/buckets/s3_bucket"
+  autoload :GCSBucket, "asset_cloud/buckets/gcs_bucket"
 
-# GCS
-require "asset_cloud/buckets/gcs_bucket"
+  class << self
+    def eager_load!
+      _ = Base
+      _ = S3Bucket
+      _ = GCSBucket
+    end
+  end
+end
 
 # Extensions
-require "asset_cloud/free_key_locator"
 require "asset_cloud/callbacks"
 require "asset_cloud/validations"
 
 require "asset_cloud/asset_extension"
-
-AssetCloud::Base.class_eval do
-  include AssetCloud::FreeKeyLocator
-  include AssetCloud::Callbacks
-  callback_methods :write, :delete
-end
 
 AssetCloud::Asset.class_eval do
   include AssetCloud::Callbacks
@@ -63,4 +66,8 @@ AssetCloud::Asset.class_eval do
       add_error("#{key.inspect} contains illegal characters")
     end
   end
+end
+
+if defined?(Rails::Railtie)
+  require "asset_cloud/railtie"
 end
